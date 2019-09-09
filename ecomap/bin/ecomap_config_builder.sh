24010b92e2aac728eca83ef00d53a5b0a4ba563b
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-09 17:43:54 +0300

    Update ecomap_config_builder.sh

--- ecomap/bin/ecomap_config_builder.sh
+++ ecomap/bin/ecomap_config_builder.sh
@@ -1,7 +1,7 @@
 # !/usr/bin/env bash
 # Script run config builder 
 
-PRODROOT=~/test_ecomap/ecomap
+PRODROOT=~/build/severyn261/test_ecomap/ecomap
 PYSRCROOT=${PRODROOT}/src/python
 CONFROOT=${PRODROOT}/etc
 PYTHONPATH=$PYSRCROOT
