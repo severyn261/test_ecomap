b608eb515a00dc829d2989c7bb2d76b311309a25
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-10 13:49:32 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get update
 sudo apt-get install -y apache2
+sudo apt-get install mysql-server -y
+sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install libapache2-mod-wsgi -y 
 sudo apt-get install libapache2-mod-wsgi python-dev -y 
 sudo apt-get install memcached -y
