163a32233a7726dc7f627bfbaf9651becc2488cb
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-12 19:45:07 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -12,8 +12,13 @@ sudo apt-get install libxml2-dev libxslt-dev libssl-dev-y  && echo 'success' && 
 sudo apt-get install libxml2-dev libxslt-dev python-dev -y  && echo 'success' && sleep 3s;
 sudo apt-get install libgirepository1.0-dev -y  && echo 'success' && sleep 3s;
 sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev -y  && echo 'success' && sleep 3s;
+sudo apt-get install libhashkit2 libhashkit-dev -y && echo 'success' && sleep 3s;
+sudo apt-get install libmemcached-dev -y  && echo 'success' && sleep 3s;
+sudo apt-get install libmemcached11 -y  && echo 'success' && sleep 3s;
+sudo apt-get install libmemcachedutil2 -y  && echo 'success' && sleep 3s;
+sudo apt-get install libsasl2-dev -y && echo 'success' && sleep 3s;
 
-# sudo pip install -r requirements.txt
+sudo pip install -r requirements.txt
 
 #sudo bash ~/home/workspace/test_ecomap/install/run.sh
 
