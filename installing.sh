165b085b5f02a8b77c0c8fcd6039de92ae6b1598
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-12 19:02:54 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install mysql-server -y
-sudo apt-get install libmysqlclient-dev -y && sudo bash ecomap/db/ecomap/dump.sh
-sudo apt-get install -y apache2
-sudo apt-get install libapache2-mod-wsgi -y
-sudo apt-get install python-dev -y
-sudo apt-get install memcached -y
-sudo apt-get install python-pip -y
-sudo apt-get install libxml2-dev libxslt-dev libssl-dev-y
-sudo apt-get install libxml2-dev libxslt-dev python-dev -y
-sudo apt-get install libgirepository1.0-dev -y
-sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev -y
+sudo apt-get update && echo 'success' && sleep 3s;
+sudo apt-get install mysql-server -y && echo 'success' && sleep 3s;
+sudo apt-get install libmysqlclient-dev -y && sudo bash ecomap/db/ecomap/dump.sh  && echo 'success' && sleep 3s;
+sudo apt-get install -y apache2  && echo 'success' && sleep 3s;
+sudo apt-get install libapache2-mod-wsgi -y  && echo 'success' && sleep 3s;
+sudo apt-get install python-dev -y  && echo 'success' && sleep 3s;
+sudo apt-get install memcached -y  && echo 'success' && sleep 3s;
+sudo apt-get install python-pip -y  && echo 'success' && sleep 3s;
+sudo apt-get install libxml2-dev libxslt-dev libssl-dev-y  && echo 'success' && sleep 3s;
+sudo apt-get install libxml2-dev libxslt-dev python-dev -y  && echo 'success' && sleep 3s;
+sudo apt-get install libgirepository1.0-dev -y  && echo 'success' && sleep 3s;
+sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev -y  && echo 'success' && sleep 3s;
 
-sudo pip install -r requirements.txt
+#sudo pip install -r requirements.txt
 
 sudo bash ~/home/workspace/test_ecomap/install/run.sh
 
