5f6f921724504b2045d231936b193a6f85e25606
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-12 19:20:40 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -3,7 +3,8 @@
 sudo apt-get update && echo 'success' && sleep 3s;
 sudo apt-get install mysql-server -y && echo 'success' && sleep 3s;
 sudo apt-get install libmysqlclient-dev -y && sudo bash ecomap/db/ecomap/dump.sh  && echo 'success' && sleep 3s;
-sudo apt-get install -y apache2  && echo 'success' && sleep 3s;
+sudo apt-get install -y apache2 && echo 'success' && sleep 3s;
+sudo apt-get install libapache2-mod-wsgi && echo 'success' && sleep 3s;
 sudo apt-get install libapache2-mod-wsgi -y  && echo 'success' && sleep 3s;
 sudo apt-get install python-dev -y  && echo 'success' && sleep 3s;
 sudo apt-get install memcached -y  && echo 'success' && sleep 3s;
