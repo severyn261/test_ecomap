8b115b0280e1fdd926c666a12bf2ad0fee57b456
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-12 09:08:18 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install mysql-server -y
-sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install libmysqlclient-dev -y && sudo bash ecomap/db/ecomap/dump.sh
 sudo apt-get install -y apache2
 sudo apt-get install libapache2-mod-wsgi -y
 sudo apt-get install python-dev -y
@@ -12,3 +12,5 @@ sudo apt-get install libxml2-dev libxslt-dev libssl-dev-y
 sudo apt-get install libxml2-dev libxslt-dev python-dev -y
 sudo apt-get install libgirepository1.0-dev -y
 sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev -y
+
+sudo pip install -r requirements.txt
