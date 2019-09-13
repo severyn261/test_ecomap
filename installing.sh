8322a62c9a119dfe38684ee7e89c4266f625c04a
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-13 14:08:50 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-sudo apt-get update && echo 'success' && sleep 3s;
-sudo apt-get install mysql-server -y && echo 'success' && sleep 3s;
-sudo apt-get install libmysqlclient-dev -y && sudo bash ecomap/db/ecomap/dump.sh  && echo 'success' && sleep 3s;
-sudo apt-get install -y apache2 && echo 'success' && sleep 3s;
-sudo apt-get install libapache2-mod-wsgi -y && echo 'success' && sleep 3s;
-sudo apt-get install libapache2-mod-wsgi python-dev -y  && echo 'success' && sleep 3s;
-sudo apt-get install memcached -y  && echo 'success' && sleep 3s;
-sudo apt-get install python-pip -y  && echo 'success' && sleep 3s;
-sudo apt-get install libxml2-dev libxslt-dev libssl-dev-y  && echo 'success' && sleep 3s;
-sudo apt-get install libxml2-dev libxslt-dev python-dev -y  && echo 'success' && sleep 3s;
-sudo apt-get install libgirepository1.0-dev -y  && echo 'success' && sleep 3s;
-sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev -y  && echo 'success' && sleep 3s;
-sudo apt-get install libhashkit2 libhashkit-dev -y && echo 'success' && sleep 3s;
-sudo apt-get install libmemcached-dev -y  && echo 'success' && sleep 3s;
-sudo apt-get install libmemcached11 -y  && echo 'success' && sleep 3s;
-sudo apt-get install libmemcachedutil2 -y  && echo 'success' && sleep 3s;
-sudo apt-get install libsasl2-dev -y && echo 'success' && sleep 3s;
+sudo apt-get update
+sudo apt-get install -y mysql-server \
+            libmysqlclient-dev \
+            apache2 \
+            libapache2-mod-wsgi \ 
+            libapache2-mod-wsgi python-dev \ 
+            memcached \ 
+            python-pip \ 
+            libxml2-dev \
+            libxslt-dev \
+            libssl-dev \
+            python-dev \ 
+            libgirepository1.0-dev \ 
+            libcairo2-dev \
+            libjpeg-dev \
+            libgif-dev \ 
+            libhashkit2 \
+            libhashkit-dev \
+            libmemcached-dev \ 
+            libmemcached11 \ 
+            libmemcachedutil2 \ 
+            libsasl2-dev 
+
+echo '================ SUCCSSESFULL =================='
 
 sudo pip install -r requirements.txt
 
-#sudo bash ~/home/workspace/test_ecomap/install/run.sh
 echo "Done"
