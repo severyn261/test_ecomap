3de359443dea36397048600ab8a8e845d260b1bf
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-17 10:45:55 +0300

    Update dump.sh

--- ecomap/db/ecomap/dump.sh
+++ ecomap/db/ecomap/dump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo echo '[mysqld] default-character-set = utf8 init_connect=‘SET collation_connection = utf8_unicode_ci’ character-set-server = utf8 collation-server = utf8_unicode_ci [client] default-character-set = utf8' >> /etc/mysql/my.cnf
-sudo echo "GRANT ALL PRIVILEGES ON *.* TO 'cat'@'%' IDENTIFIED BY '1qaz2wsx3edc' WITH GRANT OPTION;" | sudo mysql;
-sudo echo "GRANT ALL PRIVILEGES ON *.* TO 'dog'@'%' IDENTIFIED BY 'k3i4i5lm6m6' WITH GRANT OPTION;" | sudo mysql;
+sudo echo "GRANT ALL ON `%`.* TO cat@`%`;" | sudo mysql;
+sudo echo "GRANT ALL ON `%`.* TO dog@`%`;" | sudo mysql;
 sudo echo "FLUSH PRIVILEGES;" | sudo mysql;
 sudo echo "CREATE DATABASE ecomap_db CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | sudo mysql;
 #sudo echo "USE ecomap_db;" | sudo mysql;
