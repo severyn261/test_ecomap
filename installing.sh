be4e3834c26a0d2f99dc27108e7a9ccce659a1be
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-19 19:38:10 +0300

    Update installing.sh

--- installing.sh
+++ installing.sh
@@ -32,3 +32,4 @@ sudo sed -i "s/ecomapIP/$(echo "$(hostname -I)" | sed 's/^2.//;s/.$//')/g" /home
 sudo cp /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf /etc/apache2/sites-available/ecomap.conf
 sudo a2ensite ecomap
 sudo systemctl reload apache2
+sudo echo "ALL TASKS ARE OK" > /home/ubuntu/README.TXT
