1507070e37f21650ff4a5cdbb9617045e5090520
root <root@ip-172-31-11-51.us-east-2.compute.internal>
2019-09-19 09:39:40 +0000

    change

--- installing.sh
+++ installing.sh
@@ -4,27 +4,27 @@ sudo apt-get update
 sudo apt-get install -y mysql-server \
             libmysqlclient-dev \
             apache2 \
-            libapache2-mod-wsgi \ 
-            libapache2-mod-wsgi python-dev \ 
-            memcached \ 
-            python-pip \ 
+            libapache2-mod-wsgi \
+            libapache2-mod-wsgi python-dev \
+            memcached \
+            python-pip \
             libxml2-dev \
             libxslt-dev \
             libssl-dev \
-            python-dev \ 
-            libgirepository1.0-dev \ 
+            python-dev \
+            libgirepository1.0-dev \
             libcairo2-dev \
             libjpeg-dev \
-            libgif-dev \ 
+            libgif-dev \
             libhashkit2 \
             libhashkit-dev \
-            libmemcached-dev \ 
-            libmemcached11 \ 
-            libmemcachedutil2 \ 
-            libsasl2-dev 
+            libmemcached-dev \
+            libmemcached11 \
+            libmemcachedutil2 \
+            libsasl2-dev
 
 echo '================ SUCCSSESFULL =================='
 
-sudo pip install -r requirements.txt
+sudo pip install -r /home/ubuntu/home/workspace/test_ecomap/requirements.txt
 
 echo "Done"
