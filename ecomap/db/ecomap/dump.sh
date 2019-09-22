547b6414d538c434b55ae2b7320b5af56f5c8197
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-22 22:48:12 +0300

    Update dump.sh

--- ecomap/db/ecomap/dump.sh
+++ ecomap/db/ecomap/dump.sh
@@ -3,6 +3,6 @@
 sudo echo "GRANT ALL ON `%`.* TO cat@`%`;" | mysql -h address -P portdb -u username -puserpass;
 sudo echo "GRANT ALL ON `%`.* TO dog@`%`;" | mysql -h address -P portdb -u username -puserpass;
 sudo echo "FLUSH PRIVILEGES;" | mysql -h address -P portdb -u username -puserpass;
-sudo echo "CREATE DATABASE ecomap_db CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -h address -P portdb -u username -puserpass;
+sudo echo "CREATE DATABASE dbname CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -h address -P portdb -u username -puserpass;
 sudo mysql -h address -P portdb -u username -puserpass dbname < /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/CREATE_DB.sql
 sudo mysql -h address -P portdb -u username -puserpass dbname < /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/INSERT_DATA.sql
