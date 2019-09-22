07b6b06abbec3027d1c265774d6ebba87f044d5c
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-22 22:41:41 +0300

    Update dump.sh

--- ecomap/db/ecomap/dump.sh
+++ ecomap/db/ecomap/dump.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo echo '[mysqld] default-character-set = utf8 init_connect=‘SET collation_connection = utf8_unicode_ci’ character-set-server = utf8 collation-server = utf8_unicode_ci [client] default-character-set = utf8' >> /etc/mysql/my.cnf
-sudo echo "GRANT ALL ON `%`.* TO cat@`%`;" | sudo mysql;
-sudo echo "GRANT ALL ON `%`.* TO dog@`%`;" | sudo mysql;
-sudo echo "FLUSH PRIVILEGES;" | sudo mysql;
-sudo echo "CREATE DATABASE ecomap_db CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | sudo mysql;
-#sudo echo "USE ecomap_db;" | sudo mysql;
-sudo mysql ecomap_db < /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/CREATE_DB.sql
-sudo mysql ecomap_db < /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/INSERT_DATA.sql
+sudo echo "GRANT ALL ON `%`.* TO cat@`%`;" | mysql -h address -P portdb -u username -puserpass;
+sudo echo "GRANT ALL ON `%`.* TO dog@`%`;" | mysql -h address -P portdb -u username -puserpass;
+sudo echo "FLUSH PRIVILEGES;" | mysql -h address -P portdb -u username -puserpass;
+sudo echo "CREATE DATABASE ecomap_db CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -h address -P portdb -u username -puserpass;
+sudo mysql -h address -P portdb -u username -puserpass ecomap_db < /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/CREATE_DB.sql
+sudo mysql -h address -P portdb -u username -puserpass ecomap_db < /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/INSERT_DATA.sql
