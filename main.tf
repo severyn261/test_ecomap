3210733d3a32238cb3b43bfccb40b25e95b81939
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-23 18:09:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,35 +7,35 @@ provider "aws" {
 ################################################################################
 #                                  INSTANCE                                    #
 ################################################################################
-resource "aws_instance" "Sev_ubuntu" {
-  ami                    = "${var.ami}"
-  instance_type          = "${var.instance_type}"
-  availability_zone      = "${var.availability_zone}"
-  count                  = "${var.count_inst}"
-  vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
-  subnet_id              = "${var.subnet_id}"
-  security_groups        = ["${var.security_group_ids}"]
-  key_name               = "${var.key_acc}"
-  user_data              = "${file("testrun.sh")}"
-  monitoring             = false
-  root_block_device {
-    volume_type           = "gp2"
-    volume_size           = 10
-    delete_on_termination = true
-  }
+# resource "aws_instance" "Sev_ubuntu" {
+#   ami                    = "${var.ami}"
+#   instance_type          = "${var.instance_type}"
+#   availability_zone      = "${var.availability_zone}"
+#   count                  = "${var.count_inst}"
+#   vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
+#   subnet_id              = "${var.subnet_id}"
+#   security_groups        = ["${var.security_group_ids}"]
+#   key_name               = "${var.key_acc}"
+#   user_data              = "${file("testrun.sh")}"
+#   monitoring             = false
+#   root_block_device {
+#     volume_type           = "gp2"
+#     volume_size           = 10
+#     delete_on_termination = true
+#   }
 
-  volume_tags = {
-    "${var.tag-name}" = "${var.tag-value}"
-  }
+#   volume_tags = {
+#     "${var.tag-name}" = "${var.tag-value}"
+#   }
 
-  associate_public_ip_address = true
-  source_dest_check           = false
+#   associate_public_ip_address = true
+#   source_dest_check           = false
 
-  tags = {
-    Name              = "${var.name_inst}-${count.index + 1}"
-    "${var.tag-name}" = "${var.tag-value}"
-  }
-}
+#   tags = {
+#     Name              = "${var.name_inst}-${count.index + 1}"
+#     "${var.tag-name}" = "${var.tag-value}"
+#   }
+# }
 ################################################################################
 #                              Security Group                                  #
 # ##############################################################################
