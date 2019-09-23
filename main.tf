c083076b468c49a1c9a772c85ea5d3e116bc8564
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-23 21:06:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,14 +65,14 @@ provider "aws" {
 #                                     EBS                                      #
 ################################################################################
 resource "aws_elastic_beanstalk_application" "tftest" {
-  name = "${var.app_name}"
+  name = "${var.app_name}-${TRAVIS_BUILD_NUMBER}"
   tags = {
     "${var.tag-name}" = "${var.tag-value}"
   }
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "ecomap-name-travis"
+  name                = "ecomap-TRAVIS-${TRAVIS_BUILD_NUMBER}"
   application         = "${aws_elastic_beanstalk_application.tftest.name}"
   solution_stack_name = "${var.solution_stack_name}"
 
