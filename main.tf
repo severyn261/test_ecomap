e3527c4e588a96464a0944ec9efe23cfc3e4c3dd
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-23 19:06:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_security_group" "Sev_sgubuntu" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name              = "SevNewSG"
+    Name              = "SevNewSG4app"
     "${var.tag-name}" = "${var.tag-value}"
   }
 }
