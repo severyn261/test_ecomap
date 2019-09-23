f2e8d7b6dd0d0ed0fe33ce0313ec4c8c08da90ae
Severyn Vashchuk <39495187+severyn261@users.noreply.github.com>
2019-09-23 09:55:57 +0300

    Update and rename installingorig.sh to installing.sh

--- installing.sh
+++ installing.sh
@@ -24,12 +24,12 @@ sudo apt-get install -y mysql-server \
             libsasl2-dev\
             wget\
             unzip
-sudo echo "Packages installed" > /home/ubuntu/README.TXT
+echo "Packages installed" > /home/ubuntu/README.TXT
 sudo pip install -r /home/ubuntu/home/workspace/test_ecomap/requirements.txt
-sudo echo "pip packages installed" >> /home/ubuntu/README.TXT
+echo "pip packages installed" >> /home/ubuntu/README.TXT
 sudo bash /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/dump.sh
 # sudo rm /home/ubuntu/home/workspace/test_ecomap/ecomap/db/ecomap/dump.sh
-sudo echo "db dumped" >> /home/ubuntu/README.TXT
+echo "db dumped" >> /home/ubuntu/README.TXT
 sleep 10s
 sudo a2dissite 000-default
 sudo systemctl reload apache2
@@ -37,4 +37,4 @@ sudo sed -i "s/ecomapIP/$(echo "$(hostname -I)" | sed 's/^2.//;s/.$//')/g" /home
 sudo cp /home/ubuntu/home/workspace/test_ecomap/ecomap/etc/_ecomap.apache.conf /etc/apache2/sites-available/ecomap.conf
 sudo a2ensite ecomap
 sudo systemctl reload apache2
-sudo echo "ALL TASKS ARE OK" > /home/ubuntu/README.TXT
+echo "ALL TASKS ARE OK" > /home/ubuntu/README.TXT
